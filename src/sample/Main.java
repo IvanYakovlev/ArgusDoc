edd2c29f721ce2b7179da478a643e475c93e5d1f
ivanyakovlev <ivanyakovlev.jd@gmail.com>
2017-11-01 18:26:27 +0700

    123

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,8 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setMinHeight(687);
-        primaryStage.setMinWidth(773);
+        primaryStage.setMinHeight(715);
+        primaryStage.setMinWidth(788);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
