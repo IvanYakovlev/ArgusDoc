d3ec11ba8bfb7d6e4c3da38684119f6e8bf29b67
Ivan Yakovlev <ivanyakovlev.jd@gmail.com>
2017-11-29 17:08:34 +0700

    Maven on

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
-package sample;
 /*
-* venom*/
+*Ivan Yakovlev*/
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("Main_window.fxml"));
+        primaryStage.setTitle("ArgusDoc");
         primaryStage.setMinHeight(715);
         primaryStage.setMinWidth(788);
         primaryStage.setScene(new Scene(root, 300, 275));
