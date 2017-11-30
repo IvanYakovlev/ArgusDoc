76dfe27c6a012bac32668ea91b781fe6286b4fd7
Ivan Yakovlev <ivanyakovlev.jd@gmail.com>
2017-11-30 18:05:23 +0700

    icon

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,6 +17,7 @@ public class Main extends Application {
         primaryStage.setMinWidth(1000);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        primaryStage.getIcons().add(new Image("icon.jpg"));
     }
 
 
