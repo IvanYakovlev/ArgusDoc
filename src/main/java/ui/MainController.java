71cf21508739119cc992672301f2700156ff3c99
IvanYakovlev <IvanYakovlev.jd@gmail.com>
2017-12-13 22:34:23 +0700

    UI1

--- src/main/java/ui/MainController.java
+++ src/main/java/ui/MainController.java
@@ -1,4 +1,11 @@
 package ui;
 
+import javafx.fxml.FXML;
+
+import javax.swing.text.TableView;
+import java.awt.*;
+
 public class MainController {
+    @FXML private TableView tableDepartments;
+    @FXML private TextField txtDepartment;
 }
