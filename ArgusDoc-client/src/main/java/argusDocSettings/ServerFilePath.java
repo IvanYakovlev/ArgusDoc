85bdbfa55d6322dcfc237df890348616ff57f94e
IvanYakovlev <IvanYakovlev.jd@gmail.com>
2018-02-25 20:54:45 +0700

    ff

--- ArgusDoc-client/src/main/java/argusDocSettings/ServerFilePath.java
+++ ArgusDoc-client/src/main/java/argusDocSettings/ServerFilePath.java
@@ -15,3 +15,24 @@ public class ServerFilePath {
     public static final String LETTERS_FILE_PATH = "E:/ArgusDoc/Letters/";
     public static final String TASKS_FILE_PATH = "E:/ArgusDoc/Tasks/";
 }
+/*
+*.class
+# Log file
+        *.log
+
+        # BlueJ files
+        *.ctxt
+
+        # Mobile Tools for Java (J2ME)
+        .mtj.tmp/
+
+        # Package Files #
+        *.jar
+        *.war
+        *.ear
+        *.zip
+        *.tar.gz
+        *.rar
+
+        # virtual machine crash logs, see http://www.java.com/en/download/help/error_hotspot.xml
+        hs_err_pid**/
