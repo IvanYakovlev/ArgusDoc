e64df8b3edf0f14c6940532e70397b8b1efb66c3
IvanYakovlev <ivanyakovlev.jd@gmail.com>
2018-03-22 23:12:24 +0700

    ff

--- ArgusDoc-client/src/main/java/controller/AddTaskController.java
+++ ArgusDoc-client/src/main/java/controller/AddTaskController.java
@@ -8,6 +8,7 @@ import entity.TaskEntity;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ButtonType;
+import org.controlsfx.control.CheckComboBox;
 import service.*;
 import dialog.ADInfo;
 import javafx.event.ActionEvent;
@@ -39,7 +40,7 @@ public class AddTaskController {
     @FXML
     private JFXTextField txtTaskName;
     @FXML
-    private JFXComboBox<String> comboBoxEmployee = new JFXComboBox<String>();
+    private CheckComboBox<String> checkComboBoxEmployee = new CheckComboBox<String>();
     @FXML
     private JFXDatePicker datePickerTask;
     @FXML
@@ -58,83 +59,90 @@ public class AddTaskController {
     private TaskService taskService = ServiceRegistry.taskService;
     private EventService eventService = ServiceRegistry.eventService;
 
+    TaskEntity taskEntity;
     public  void initialize() throws RemoteException {
 
         employeeService.listEmployees();
         ObservableList<String> observableListEmployeesName = FXCollections.observableArrayList(employeeService.listEmployeesName());
-        comboBoxEmployee.setItems(observableListEmployeesName);
+        checkComboBoxEmployee.getItems().setAll(observableListEmployeesName);
         timePickerTask.setIs24HourView(true);
+
 
     }
 
     public void addTaskButton(ActionEvent actionEvent) throws IOException {
-        if (txtTaskName.getText().isEmpty() ||  comboBoxEmployee.getValue()==null || datePickerTask.getValue()==null||textAreaTask.getText().isEmpty()) {
+        if (txtTaskName.getText().isEmpty() || checkComboBoxEmployee.getCheckModel().getCheckedItems() == null || datePickerTask.getValue() == null || textAreaTask.getText().isEmpty()) {
             ADInfo.getAdInfo().dialog(Alert.AlertType.WARNING, "Не все поля заполнены!");
         } else {
 
 
-            TaskEntity taskEntity = new TaskEntity();
-            taskEntity.setTaskName(txtTaskName.getText());
-            taskEntity.setTaskText(textAreaTask.getText());
-            taskEntity.setTaskAttachmentFile(attachmentFile);
-            if (attachmentFile!=null) {
-                taskEntity.setTaskAttachment(ServerFilePath.TASKS_FILE_PATH + attachmentFile.getName());
+
+
+//создаем задачи
+            for (int i = 0; i < checkComboBoxEmployee.getCheckModel().getCheckedItems().size(); i++) {
+                taskEntity = new TaskEntity();
+                taskEntity.setTaskName(txtTaskName.getText());
+                taskEntity.setTaskText(textAreaTask.getText());
+                taskEntity.setTaskAttachmentFile(attachmentFile);
+                if (attachmentFile != null) {
+                    taskEntity.setTaskAttachment(ServerFilePath.TASKS_FILE_PATH + attachmentFile.getName());
+                }
+                taskEntity.setTaskFromEmployee(authorizedUser.getEmployeeName());
+                taskEntity.setEmployeeId(employeeService.getIdEmployeeByName(checkComboBoxEmployee.getCheckModel().getCheckedItems().get(i)));
+                taskEntity.setTaskTerm(java.sql.Date.valueOf(datePickerTask.getValue()));
+                taskEntity.setTaskTime(java.sql.Time.valueOf(timePickerTask.getValue()));
+                taskEntity.setStatusTaskId(StatusTask.NOT_DONE);
+                taskEntity.setTaskIsLetter(0);
+                taskEntity.setLetterId(0);
+
+
+                try {
+                    taskService.addTask(taskEntity);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
-            taskEntity.setTaskFromEmployee(authorizedUser.getEmployeeName());
-            taskEntity.setEmployeeId(employeeService.getIdEmployeeByName(comboBoxEmployee.getValue()));
-            taskEntity.setTaskTerm(java.sql.Date.valueOf(datePickerTask.getValue()));
-            taskEntity.setTaskTime(java.sql.Time.valueOf(timePickerTask.getValue()));
-            taskEntity.setStatusTaskId(StatusTask.NOT_DONE);
-            taskEntity.setTaskIsLetter(0);
-            taskEntity.setLetterId(0);
+//загружаем файл
+                try {
+                    if (taskEntity.getTaskIsLetter() == 0 && taskEntity.getTaskAttachmentFile() != null) {
+                        File destFile = new File(taskEntity.getTaskAttachment());
+                        Files.copy(taskEntity.getTaskAttachmentFile().toPath(), destFile.toPath());
+                    }
+                } catch (IOException e) {
+
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                    //alert.setTitle("Delete File");
+                    alert.setHeaderText("Файл с таким именем уже существует! Хотите заменить?");
 
 
-            try {
-                taskService.addTask(taskEntity);
-                if (taskEntity.getTaskIsLetter()==0&& taskEntity.getTaskAttachmentFile()!=null) {
-                    File destFile = new File(taskEntity.getTaskAttachment());
-                    Files.copy(taskEntity.getTaskAttachmentFile().toPath(), destFile.toPath());
+                    // option != null.
+                    Optional<ButtonType> option = alert.showAndWait();
+
+                    if (option.get() == null) {
+
+                    } else if (option.get() == ButtonType.OK) {
+                        Path path = Paths.get(taskEntity.getTaskAttachment());
+                        Files.delete(path);
+
+                        if (taskEntity.getTaskIsLetter() == 0 && taskEntity.getTaskAttachmentFile() != null) {
+                            File destFile = new File(taskEntity.getTaskAttachment());
+                            Files.copy(taskEntity.getTaskAttachmentFile().toPath(), destFile.toPath());
+
+                        }
+
+                    } else if (option.get() == ButtonType.CANCEL) {
+
+                    } else {
+
+                    }
                 }
 
+                Stage stage = (Stage) addTaskButton.getScene().getWindow();
+                stage.close();
 
-            }  catch (SQLException e) {
-                 e.printStackTrace();
-            } catch (IOException e) {
-
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            //alert.setTitle("Delete File");
-                alert.setHeaderText("Файл с таким именем уже существует! Хотите заменить?");
-
-
-            // option != null.
-                 Optional<ButtonType> option = alert.showAndWait();
-
-                  if (option.get() == null) {
-
-                  } else if (option.get() == ButtonType.OK) {
-                       Path path = Paths.get(taskEntity.getTaskAttachment());
-                       Files.delete(path);
-
-                     if (taskEntity.getTaskIsLetter()==0&& taskEntity.getTaskAttachmentFile()!=null) {
-                         File destFile = new File(taskEntity.getTaskAttachment());
-                         Files.copy(taskEntity.getTaskAttachmentFile().toPath(), destFile.toPath());
-
-                }
-
-            } else if (option.get() == ButtonType.CANCEL) {
-
-            } else {
-
-            }
-        }
-
-
-            Stage stage = (Stage) addTaskButton.getScene().getWindow();
-            stage.close();
 
         }
     }
-
     public void cancelAddTaskButton(ActionEvent actionEvent) {
         Stage stage = (Stage) cancelAddTaskButton.getScene().getWindow();
         stage.close();
